2143f9eaac6b9ef542b93d89fbe6607f55a5dec4
James <jamesalexanderlevine@gmail.com>
2020-11-09 12:11:14 +0000

    add redirects file and build

---
+++ build/precache-manifest.7c771a51379a4ed7a5f1d3191c85e159.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7908bb4bb2c6453a07c010cf4958cdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7506ac324024eb0ff2fd",
+    "url": "/static/css/2.0024d7e8.chunk.css"
+  },
+  {
+    "revision": "889b472c4d5a301fe443",
+    "url": "/static/css/main.ca472ab4.chunk.css"
+  },
+  {
+    "revision": "7506ac324024eb0ff2fd",
+    "url": "/static/js/2.0f688189.chunk.js"
+  },
+  {
+    "revision": "889b472c4d5a301fe443",
+    "url": "/static/js/main.8785e290.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
