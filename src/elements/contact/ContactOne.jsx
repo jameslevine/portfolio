16f5747707844d0b60f1fb911feed86f08050982
James <jamesalexanderlevine@gmail.com>
2020-11-09 21:48:49 +0000

    change netlify form

--- src/elements/contact/ContactOne.jsx
+++ src/elements/contact/ContactOne.jsx
@@ -21,7 +21,7 @@ class ContactOne extends Component{
         fetch("/", {
           method: "POST",
           headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: encode({ "form-name": "contact", ...this.state })
+          body: encode({ "form-name": "enquiry", ...this.state })
         })
           .then(() => alert("Success!"))
           .catch(error => alert(error));
