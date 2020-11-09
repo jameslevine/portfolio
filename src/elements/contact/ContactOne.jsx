444d3f0903ff0dcd10bc6dc8e2ce46de8b0a267d
James <jamesalexanderlevine@gmail.com>
2020-11-09 20:41:47 +0000

    change netlify form

--- src/elements/contact/ContactOne.jsx
+++ src/elements/contact/ContactOne.jsx
@@ -22,7 +22,7 @@ class ContactOne extends Component{
                             </div>
                             <div className="form-wrapper">
 
-                                <form form-name="contactUsForm" action="/" method="POST" data-netlify="true" enctype="application/x-www-form-urlencoded">
+                                <form name="contact" method="POST" data-netlify="true" enctype="application/x-www-form-urlencoded">
                                     <label htmlFor="item01">
                                         <input
                                             type="text"
